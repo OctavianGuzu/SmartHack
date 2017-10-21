4cd853328675cd3f05bd0073d7676ba19c491f7c
Octavian Guzu <octavian.guzu@gmail.com>
2017-10-21 18:49:16 +0300

    Small functionality

--- public/js/ng/root.js
+++ public/js/ng/root.js
@@ -2,8 +2,24 @@ var root = angular.module("root", ['ngRoute']);
 
 root.controller("loginController", ["$scope", "$http",function( $scope, $http ) {
 	$('#LoginBtn').click(function (e) {
-		window.location.href = '/dashboard';
-		return false;
+		var email = $('#InputEmail').val();
+		var pass = $('#InputPassword').val();
+
+		if(email != "" && pass != "") {
+			$scope.checkLoogin(email, pass);
+		}
+
+		//window.location.href = '/dashboard';
+		//return false;
 	})
+
+	$scope.checkLoogin = function(email, pass) {
+		var url = "/checkLogin?email=" + email + "&pass=" + pass;
+
+		$http.get(url)
+			.then(function (response) {
+				//console.log(response);
+			})
+	}
 
 }]);
--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,16 @@ router.get('/dashboard', function (req, res) {
 	res.render('index');
 });
 
+router.get('/checkLogin', function (req, res) {
+	console.log(req.query.email);
+
+	var response = {
+		status_code : 0,
+        status_message : "success",
+        data : null
+	};
+
+	res.json(response);
+});
+
 module.exports = router;
